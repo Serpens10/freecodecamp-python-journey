4e1aa8a5c332c6d8a8e940c0c24cedeb0dca5b1c
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-10-20 18:09:19 +0100

    exercise 3 based on lesson

---
+++ exercises 3 & 4.py
@@ -0,0 +1,11 @@
+# try and except 
+
+rawstr=input('enter a number: ')
+try :
+    ival = int(rawstr)
+except :
+    ival = -1
+if ival > 0 :    
+    print ('nice work')    
+else :
+    print ('not a number')
