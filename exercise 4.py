51ee662bd356ebd8cc84ea2bfd232d7c84843788
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-10-20 18:47:04 +0100

    more detailed report
    show regular and overtime pay separately and then total

--- exercise 4.py
+++ exercise 4.py
@@ -3,10 +3,15 @@ rate= input('Enter rate: ')
 ihours= float(hours)
 irate= float(rate)
 if ihours > 40 :
+    
     extrahours= ihours-40
     extrarate= irate * 1.5
-    pay= (40 * irate) + (extrahours * extrarate)
-    print ('your pay is: ', pay)
+
+    regular= 40 * irate
+    overtime= extrahours * extrarate
+    print ('your regular pay is: ', regular, 'your overtime pay is: ', overtime)
+    total= regular + overtime
+    print ('Your total pay is: ', total)
 else :
     pay= ihours * irate
     print ('your pay is: ', pay)    
