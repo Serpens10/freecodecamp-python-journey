4adb1d042be0a4e9dbabf87d27acfe1d764a40c3
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-10-21 17:07:06 +0100

    added try blocks

--- exercise 4.2.py
+++ exercise 4.2.py
@@ -1,9 +1,6 @@
 
 hours= input('Enter hours: ')
 rate= input('Enter rate: ')
-hours= float(hours)
-rate= float(rate)
-
 def computepay (hours, rate):
     if hours > 40 :
         
@@ -14,37 +11,20 @@ def computepay (hours, rate):
 
     return pay   
 
-x= computepay (hours, rate)
-print ('Your pay is: ', x)
+try :
+    hours= float(hours)
+    try :
+        rate= float(rate)
+        x= computepay (hours, rate)
+        print ('Your pay is: ', x)
+    except :
+        hours==-1    
+        print ('Error, please enter numeric input') 
+except :
+    rate==-1
+    print ('Error, please enter numeric input')   
     
 
-
-
-# hours= input('Enter hours: ')
-# rate= input('Enter rate: ')
-# try :
-#     ihours= float(hours)
-#     try :
-#         irate= float(rate)
-#         if ihours > 40 :
-    
-#             extrahours= ihours-40
-#             extrarate= irate * 1.5
-
-#             regular= 40 * irate
-#             overtime= extrahours * extrarate
-#             print ('your regular pay is: ', regular, 'your overtime pay is: ', overtime)
-#             total= regular + overtime
-#             print ('Your total pay is: ', total)
-#         else :
-#             pay= ihours * irate
-#             print ('your pay is: ', pay)
-#     except : 
-#         irate==-1    
-#         print ('Error, please enter numeric input')          
-# except :
-#     ihours= -1
-#     print ('Error, please enter numeric input')
 
 
     
