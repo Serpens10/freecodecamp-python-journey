b4f230626caed33acd898a38c81e3a4bb6d4e98f
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-10-27 13:08:59 +0100

    defined first function
    with try and except in order to accept decimals instead of integers only.

--- learning project.py
+++ learning project.py
@@ -1,9 +1,15 @@
 def deposit():
-    while True:
+    while True :
         amount = input("Enter the amount you want to deposit $ ")
-        if amount.isdigit():
+        try :
             amount = float(amount)
             if amount > 0 :
                 break
             else :
                 print("Amount must be greater than 0.")
+        except ValueError :
+            print("Please enter amount in numerical form.")
+        
+    return amount      
+
+deposit() 
