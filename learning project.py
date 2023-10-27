a1a17dd951b10bcede1212232e7385531180b146
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-10-27 13:44:39 +0100

    defined second function

--- learning project.py
+++ learning project.py
@@ -1,6 +1,8 @@
+MAX_LINES = 3
+
 def deposit():
     while True :
-        amount = input("Enter the amount you want to deposit $ ")
+        amount = input("Enter the amount you want to deposit: $")
         try :
             amount = float(amount)
             if amount > 0 :
@@ -10,6 +12,26 @@ def deposit():
         except ValueError :
             print("Please enter amount in numerical form.")
         
-    return amount      
+    return amount   
 
-deposit() 
+def get_number_of_lines() :
+       
+    while True :
+        lines = input("What is he number of lines you want to bet on (1-" + str(MAX_LINES) + ")? ")
+        try :
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES :
+                break
+            else :
+                print("Enter a valid number of lines.")
+        except ValueError :
+            print("Please enter number of lines in numerical form.")
+        
+    return lines 
+
+def main() :
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main ()    
