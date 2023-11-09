d630147d25fa5febed57ea280be654e838f38603
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-11-09 13:03:02 +0100

    Update exercise 5.py

--- exercise 5.py
+++ exercise 5.py
@@ -4,3 +4,11 @@ count = 0
 for item in [6,234,567,9,563,67,80,0,98,65,34,2]:
     count = count + 1
     print(count, item)
+
+# total count
+
+total = 0
+for item in [6,234,567,9,563,67,80,0,98,65,34,2]:
+    total= total + item
+    print(total, item)
+print("the total is: ", total)    
