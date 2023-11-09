9fc95edaca3a9953e8f9854ed41cd208b8359631
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-11-09 13:16:43 +0100

    Update exercise 5.py

--- exercise 5.py
+++ exercise 5.py
@@ -11,4 +11,16 @@ total = 0
 for item in [6,234,567,9,563,67,80,0,98,65,34,2]:
     total= total + item
     print(total, item)
-print("the total is: ", total)    
+print("the total is: ", total)   
+
+count = 0
+sum = 0
+howmany = 0
+for number in [6,234,567,9,463,67,80,0,98,65,134,2]:
+    count = count + 1
+    if number < 100 :
+        howmany = howmany + 1
+        sum = sum + number
+        print("position:", count, "value:", number, "totaling to:", sum)
+print(f"we have {howmany} small numbers. they add up to {sum}")        
+        
