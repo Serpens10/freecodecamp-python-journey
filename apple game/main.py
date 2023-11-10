b1e38da12752cf314a0c3a339c97dae67486b33f
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-11-10 15:59:04 +0100

    add player movement

--- apple game/main.py
+++ apple game/main.py
@@ -3,6 +3,7 @@ import sys
 
 pygame.init()
 screen = pygame.display.set_mode((350, 600))
+screen_rect = screen.get_rect()
 clock = pygame.time.Clock()
 
 # constants
@@ -19,6 +20,16 @@ player_image = pygame.transform.scale(player_image, (TILESIZE, TILESIZE*2))
 player_rect = player_image.get_rect(center = (screen.get_width()/2,
                                     screen.get_height()-floor_image.get_height()-player_image.get_height()/2))
 
+def update():
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= 8
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += 8
+
+    player_rect.clamp_ip(screen_rect)    
+
 running = True 
 
 def draw():
@@ -34,6 +45,7 @@ while running :
             pygame.quit()
             sys.exit()
 
+    update()
     draw()
     clock.tick(60)        
     pygame.display.update()
