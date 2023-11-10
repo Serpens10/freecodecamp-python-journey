a30f175f673909e359cf4189b69f3c4154d55b3f
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-11-10 15:47:35 +0100

    Update main.py

--- apple game/main.py
+++ apple game/main.py
@@ -8,15 +8,23 @@ clock = pygame.time.Clock()
 # constants
 TILESIZE = 32
 
+# floor
+floor_image = pygame.image.load('assets/floor.png').convert_alpha()
+floor_image = pygame.transform.scale(floor_image, (TILESIZE*15, TILESIZE*5))
+floor_rect = floor_image.get_rect(bottomleft = (0, screen.get_height()))
+
 # player
 player_image = pygame.image.load('assets/player_static.png').convert_alpha()
 player_image = pygame.transform.scale(player_image, (TILESIZE, TILESIZE*2))
+player_rect = player_image.get_rect(center = (screen.get_width()/2,
+                                    screen.get_height()-floor_image.get_height()-player_image.get_height()/2))
 
 running = True 
 
 def draw():
     screen.fill('lightblue')
-    screen.blit(player_image, (175,300))
+    screen.blit(player_image, player_rect)
+    screen.blit(floor_image, floor_rect)
 
 # game loop to keep screen open, create boolean variable
 
